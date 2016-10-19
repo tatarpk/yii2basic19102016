7de089aa202b3cfe419beb3207828b236f193868
tatarpk <apai.pk@gmail.com>
2016-10-19 11:18:31 +0700

    new project yii2basic19102016

---
+++ views/layouts/main.php
@@ -0,0 +1,83 @@
+<?php
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+        <?php $this->beginBody() ?>
+
+        <div class="wrap">
+            <div>
+                <img src="<?= Yii::getAlias('@web') ?>/img/header.png" class="img-responsive" alt="header" >
+            </div>
+
+            <?php
+            NavBar::begin([
+                'brandLabel' => '<span class="glyphicon glyphicon-flag"></span> Apai Pinthakup',
+                'brandUrl' => Yii::$app->homeUrl,
+                'options' => [
+                    'class' => 'navbar-inverse ',
+                ],
+            ]);
+            echo Nav::widget([
+                'options' => ['class' => 'navbar-nav navbar-right'],
+                'encodeLabels' => false,
+                'items' => [
+                    ['label' => '<span class="glyphicon glyphicon-home"></span> หน้าแรก', 'url' => ['/site/index']],
+                    ['label' => '<span class="glyphicon glyphicon-info-sign"></span> เกี่ยวกับ', 'url' => ['/site/about']],
+                    ['label' => '<span class="glyphicon glyphicon-earphone"></span> ติดต่อ', 'url' => ['/site/contact']],
+                    Yii::$app->user->isGuest ? (
+                            ['label' => '<span class="glyphicon glyphicon-log-in"></span> เข้าระบบ', 'url' => ['/site/login']]
+                            ) : (
+                            '<li>'
+                            . Html::beginForm(['/site/logout'], 'post', ['class' => 'navbar-form'])
+                            . Html::submitButton(
+                                    'Logout (' . Yii::$app->user->identity->username . ')', ['class' => 'btn btn-link']
+                            )
+                            . Html::endForm()
+                            . '</li>'
+                            )
+                ],
+            ]);
+            NavBar::end();
+            ?>
+
+            <div class="container">
+                <?=
+                Breadcrumbs::widget([
+                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                ])
+                ?>
+                <?= $content ?>
+            </div>
+        </div>
+
+        <footer class="footer">
+            <div class="container">
+                <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+
+                <p class="pull-right"><?= Yii::powered() ?></p>
+            </div>
+        </footer>
+
+        <?php $this->endBody() ?>
+    </body>
+</html>
+<?php $this->endPage() ?>
